db36bf9753af4539a07863d5ccf15b101d809468
yuichkun <yuichkun@gmail.com>
2025-01-15 16:39:11 +0900

    put a box

--- v2/src/App.tsx
+++ v2/src/App.tsx
@@ -1,10 +1,23 @@
+import { Canvas } from "@react-three/fiber";
 import "./App.css";
 
 function App() {
   return (
-    <>
-      <h1>Hello World</h1>
-    </>
+    <div id="canvas-container">
+      <Canvas>
+        <ambientLight intensity={Math.PI / 2} />
+        <spotLight
+          position={[10, 10, 10]}
+          angle={0.15}
+          penumbra={1}
+          decay={0}
+          intensity={Math.PI}
+        />
+        <mesh>
+          <boxGeometry args={[1, 1, 1]} />
+        </mesh>
+      </Canvas>
+    </div>
   );
 }
 
