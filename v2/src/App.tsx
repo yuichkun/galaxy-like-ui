6126c1bbde595676e490ed9c221bfcb8fe378e80
yuichkun <yuichkun@gmail.com>
2025-01-29 15:55:00 +0900

    add avatar card

--- v2/src/App.tsx
+++ v2/src/App.tsx
@@ -1,42 +1,57 @@
 import { Canvas, useFrame, useLoader } from "@react-three/fiber";
 import "./App.css";
-import { OrbitControls } from "@react-three/drei";
+import { OrbitControls, RoundedBox } from "@react-three/drei";
 import { useRef } from "react";
-import { Mesh, TextureLoader } from "three";
+import { Mesh, TextureLoader, DoubleSide } from "three";
 import { faker } from "@faker-js/faker";
 
-const avatarUrl = faker.image.avatarGitHub();
-const RotatingBox = () => {
+type Props = {
+  position: [number, number, number];
+  avatarUrl: string;
+};
+const Card = ({ position, avatarUrl }: Props) => {
   const texture = useLoader(TextureLoader, avatarUrl);
   const meshRef = useRef<Mesh>(null);
   useFrame(() => {
     if (meshRef.current) {
-      meshRef.current.rotation.x += 0.01;
-      meshRef.current.rotation.y += 0.016;
+      // meshRef.current.rotation.x += 0.006;
+      // meshRef.current.rotation.y += 0.011;
     }
   });
   return (
-    <mesh ref={meshRef}>
-      <boxGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial map={texture} />
+    <mesh ref={meshRef} position={position}>
+      <RoundedBox args={[1, 1, 0.02]} radius={0.05} smoothness={4}>
+        <meshPhysicalMaterial
+          map={texture}
+          opacity={0.7}
+          roughness={0.9}
+          metalness={0.5}
+          side={DoubleSide}
+          transmission={0.5}
+          clearcoat={1}
+          clearcoatRoughness={0.1}
+        />
+      </RoundedBox>
     </mesh>
   );
 };
 
 function App() {
+  // Generate URLs once at parent level
+  const avatarUrls = [
+    faker.image.avatarGitHub(),
+    faker.image.avatarGitHub(),
+    faker.image.avatarGitHub(),
+  ];
+
   return (
     <div id="canvas-container">
-      <Canvas>
-        <ambientLight intensity={Math.PI / 2} />
-        <spotLight
-          position={[10, 10, 10]}
-          angle={0.15}
-          penumbra={1}
-          decay={0}
-          intensity={Math.PI}
-        />
-        <RotatingBox />
-        <OrbitControls />
+      <Canvas orthographic camera={{ zoom: 200, position: [0, 0, 100] }}>
+        <ambientLight intensity={Math.PI * 2} />
+        <Card key={0} position={[0, 0, 0]} avatarUrl={avatarUrls[0]} />
+        <Card key={1} position={[-4, 0, 0]} avatarUrl={avatarUrls[1]} />
+        <Card key={2} position={[4, 0, 0]} avatarUrl={avatarUrls[2]} />
+        <OrbitControls enableZoom={false} />
       </Canvas>
     </div>
   );
